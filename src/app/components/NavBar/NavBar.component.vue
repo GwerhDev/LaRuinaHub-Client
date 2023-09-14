9ff2d104d22c239d59f577879a725b3654de5d38
Lalo Freak <lalofreak.dev@gmail.com>
2023-09-14 18:46:33 -0300

    account update system

--- src/app/components/NavBar/NavBar.component.vue
+++ src/app/components/NavBar/NavBar.component.vue
@@ -1,30 +1,7 @@
 <style scoped lang="scss" src="./NavBar.component.scss"/>
-<script setup lang="ts">
-  import { useStore } from '../../../middlewares/store';
-  import { useRouter } from 'vue-router';
-
-  import { upToTop } from '../../../utils/upToTop';
-  import { closeMenu } from '../../../helpers/menu';
-  
+<script setup lang="ts">  
   import Menu from '../Menu/Menu.component.vue';
   import BurgerMenu from '../BurgerMenu/BurgerMenu.component.vue';
-
-  const router: any = useRouter();
-  const store: any = useStore();
-
-  var inputValue: string;
-
-
-  function handleInput(e: any) {
-    inputValue = e.target.value
-  }
-
-/*   function search(e: string) {
-    store.filterItemsByTitle(e);
-    router.push(`/home`);
-    closeMenu();
-    upToTop();
-  } */
 
   function exit() {
   }
@@ -43,10 +20,6 @@
         <div class="menu-container-desk">
           <Menu></Menu>
         </div>
-<!--         <div class="searcher-container-desk">
-          <input type="text" @input="(e) => handleInput(e)">
-          <button class="search-button-desk" @click="search(inputValue)">Buscar</button>
-        </div> -->
       </ul>
     </main>
   </div>

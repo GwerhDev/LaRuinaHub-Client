311edc5f4dbba94d051128b62113d917e642436d
Lalo Freak <lalofreak.dev@gmail.com>
2023-09-16 18:16:40 -0300

    oauth system

--- src/app/components/ChooseAccount/ChooseAccount.component.vue
+++ src/app/components/ChooseAccount/ChooseAccount.component.vue
@@ -1,20 +1,18 @@
-
-import { useRouter } from 'vue-router';
 <script setup lang="ts">
   import { useRoute } from 'vue-router';
   import { useStore } from '../../../middlewares/store';
   import { computed } from 'vue';
+  import { getUserToken } from '../../../helpers';
 
   const store = useStore();
   const route: any = useRoute();
   const currentUser: any = computed(() => store.currentUser);
-  const userToken: any = computed(() => store.userToken);
   const logged: any = computed(() => currentUser.value.logged);
   const profilePic: any = computed(() => currentUser.value.userData.profilePic);
   const email: any = computed(() => currentUser.value.userData.email);
   const username: any = computed(() => currentUser.value.userData.username);
   const uri: any = "https://" + route.params.redirect_uri;
-  const redirectUrl = uri + "/#/auth?token=" + userToken.value;
+  const redirectUrl = uri + "/#/auth?token=" + getUserToken();
 
   function selectAccount() {
     window.location.href = redirectUrl;

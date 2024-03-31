15fdb91e4485139f16366db6d0ef915cdcd09e68
GwerhDev <gwerh.dev@gmail.com>
2024-03-31 19:05:42 -0300

    landing page: styling

--- src/app/components/AccountMenu/AccountMenu.component.vue
+++ src/app/components/AccountMenu/AccountMenu.component.vue
@@ -1,24 +1,34 @@
 <style scoped lang="scss" src="./AccountMenu.component.scss" />
 <script setup lang="ts">
-import { computed, ref, Ref, onMounted } from 'vue';
+import { computed, ref, Ref, onMounted, watch } from 'vue';
 import { useStore } from '../../../middlewares/store';
 import { getUserToken } from '../../../helpers';
 import { CanvasMenuFunction, closeAccountMenu, closeMenu } from '../../../helpers/menu';
+import userIcon from '../../../assets/svg/user-icon.svg';
 
 const store = useStore();
 const token: Ref = ref("");
-const currentUser: any = computed(() => store.currentUser);
-const logged: any = computed(() => currentUser.value.logged);
-const pathAccount: string = '/account/settings/';
+const currentUser = computed(() => store.currentUser);
+const logged = computed(() => currentUser.value.logged);
+const pathAccount = '/account/settings/';
+const accountUrl: Ref = ref("");
+const getAccountUrl = () => {
+  accountUrl.value = pathAccount + token.value;
+  console.log(accountUrl.value)
+};
+
+getAccountUrl();
+
+watch(token.value, () => getAccountUrl());
 
 CanvasMenuFunction("#account-menu-container");
 
-function logout() { 
-  store.logout(); 
+function logout() {
+  store.logout();
   closeMenu();
   closeAccountMenu();
-  token.value = ""
-};
+  token.value = "";
+}
 
 function select() {
   closeAccountMenu();
@@ -33,23 +43,29 @@ onMounted(() => {
 
 <template>
   <ul class="account-menu-container" id="account-menu-container">
-    <li v-if="!logged" class="mr-1 ml-1">
-      <router-link class="menu-text" to='/login' @click="select()">
+    <li v-if="!logged">
+      <router-link class="menu-text principal-button" to='/login' @click="select()">
         Iniciar sesión
       </router-link>
     </li>
-    <li v-if="!logged" class="mr-1 ml-1">
-      <router-link class="menu-text" to='/register' @click="select()">
+    <li v-if="!logged">
+      <router-link class="menu-text secondary-button" to='/register' @click="select()">
         Registrarse
       </router-link>
     </li>
-    <li v-if="logged" class="mr-1 ml-1">
-      <router-link class="menu-text" :to="pathAccount + token" @click="select()">
-        Mi cuenta
+    <li class="current-user-data" v-if="logged">
+      <h2>{{ currentUser?.userData?.username }}</h2>
+      <img :src="currentUser?.userData?.profilePic ?? userIcon" alt="">
+      <p>{{ currentUser?.userData?.role }}</p>
+    </li>
+    <div class="separator"></div>
+    <li v-if="logged">
+      <router-link class="menu-text principal-button" :to="accountUrl" @click="select()">
+        Administrar cuenta
       </router-link>
     </li>
-    <li v-if="logged" class="mr-1 ml-1">
-      <router-link class="menu-text" to='/' @click="logout()">
+    <li v-if="logged">
+      <router-link class="menu-text secondary-button" to='/' @click="logout()">
         Cerrar sesión
       </router-link>
     </li>

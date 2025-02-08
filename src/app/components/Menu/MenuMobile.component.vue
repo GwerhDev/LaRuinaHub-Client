49adc1137b5a062ca889562dc782d41453adde9f
GwerhDev <gwerh.dev@gmail.com>
2025-02-08 20:47:33 -0300

    updating new page

--- src/app/components/Menu/MenuMobile.component.vue
+++ src/app/components/Menu/MenuMobile.component.vue
@@ -4,12 +4,36 @@ import { closeMenu } from '../../../helpers/menu';
 </script>
 
 <template>
+  <div class="container-menu-mobile">
     <ul class="ul-menu-mobile">
       <li>
         <router-link class="lateral-menu-link" to="/" @click="closeMenu()">
           Página principal
         </router-link>
       </li>
+      <li>
+        <router-link class="lateral-menu-link" to="/" @click="closeMenu()">
+          Información personal
+        </router-link>
+      </li>
+      <li>
+        <router-link class="lateral-menu-link" to="/" @click="closeMenu()">
+          Datos y privadicidad
+        </router-link>
+      </li>
+      <li>
+        <router-link class="lateral-menu-link" to="/" @click="closeMenu()">
+          Seguridad
+        </router-link>
+      </li>
+      <li>
+        <router-link class="lateral-menu-link" to="/" @click="closeMenu()">
+          Pagos y suscripciones
+        </router-link>
+      </li>
+    </ul>
+    <div class="separator mt-1 mb-1"></div>
+    <ul class="ul-menu-mobile">
       <li>
         <router-link class="lateral-menu-link" to="/environment/apps" @click="closeMenu()">
           Aplicaciones
@@ -20,10 +44,6 @@ import { closeMenu } from '../../../helpers/menu';
           Soporte técnico
         </router-link>
       </li>
-      <li>
-        <router-link class="lateral-menu-link" to="/about" @click="closeMenu()">
-          About
-        </router-link>
-      </li>
     </ul>
+  </div>
 </template>

37eb75dc2b39db4491a6a7bca2b1ef7b2ac22dab
GwerhDev <gwerh.dev@gmail.com>
2025-02-08 23:52:09 -0300

    updating new page

--- src/app/pages/LandingPage.vue
+++ src/app/pages/LandingPage.vue
@@ -1,11 +1,9 @@
 <script setup lang="ts">
 import Presentation from '../components/Presentation/Presentation.component.vue';
-import ConnectedApps from "../components/ConnectedApps/ConnectedApps.component.vue"
 </script>
 
 <template>
   <main class="main-container">
     <Presentation />
-    <ConnectedApps />
   </main>
 </template>
